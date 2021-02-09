259446be65d6dbe77f433bf5b10fd96e06f342df
Sahil Malhotra <sahil061626@gmail.com>
2021-02-09 12:36:33 +0530

    angular nav bar functionality added.

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
+  //Model object for storing Login form data.
+  model:any={
 
+  }
   constructor() { }
 
   ngOnInit(): void {
+  }
+  
+  login(){
+    console.log(this.model);
   }
 
 }
